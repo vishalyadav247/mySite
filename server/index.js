83dd5489da24979409dda998ea687efbd14fd5d4
vishal yadav <vishal@progryss.com>
2024-11-22 00:43:56 +0530

    adding some more env var

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const connectDB = require('./utils/db');
+require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const Router = require('./routes/auth-route')
 
 const cookieParser = require('cookie-parser')
+const port = process.env.PORT;
+const corsOrigin = process.env.CORS_ORIGIN;
 const path = require('path')
 const _dirname = path.dirname("")
 const buildPath = path.join(_dirname,"../frontend/build")
 
 app.use(express.json());
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: corsOrigin,
   credentials: true, 
 };
 app.use(cors(corsOptions))
@@ -20,11 +23,7 @@ app.use(express.static(buildPath))
 
 app.use('/api',Router)
 
-app.get("/", async (req,res)=>{
-  res.status(200).json("home")
-})
-
 connectDB();
-app.listen(4000, () => {
-  console.log(`App listening on 4000`)
+app.listen(port, () => {
+  console.log(`App listening on ${port}`)
 })
